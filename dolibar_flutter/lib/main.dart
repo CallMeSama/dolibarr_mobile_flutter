8f2ad0dc7b00b81603a0dc87f7bdfe21a98c5f00
Aw2001 <awafall82817@gmail.com>
2023-02-10 03:09:55 +0000

    page_login_update

--- dolibar_flutter/lib/main.dart
+++ dolibar_flutter/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:dolibar_flutter/login.dart';
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'login',
-    routes: {
-      'login': (context)=>MyLogin()
-    },
-  ));
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyLogin(),
+    );
+  }
+}
