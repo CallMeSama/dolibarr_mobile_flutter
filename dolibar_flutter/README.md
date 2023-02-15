6293a3b5a85fabb6a26e3b4885a26748f6dc9336
Aw2001 <awafall82817@gmail.com>
2023-02-15 01:24:13 +0000

    page terminée

--- dolibar_flutter/README.md
+++ dolibar_flutter/README.md
@@ -1,16 +1,11 @@
-# dolibar_flutter
+<div id="header" align="center">
 
-A new Flutter project.
+# Animation Flow
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
+<p align="center">
+    <img src="video.gif" alt="Image"  />
+</p>
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+</div>
---
+++ dolibar_flutter/lib/presentation/widgets/username_field.dart
@@ -0,0 +1,146 @@
+import 'package:animated_login/styles/colors.dart';
+import 'package:flutter/material.dart';
+
+class UsernameField extends StatefulWidget {
+  final bool fadeUsername;
+  final TextEditingController usernameController;
+  const UsernameField(
+      {super.key, required this.usernameController, required this.fadeUsername});
+
+  @override
+  State<UsernameField> createState() => _UsernameFieldState();
+}
+
+class _UsernameFieldState extends State<UsernameField>
+    with SingleTickerProviderStateMixin {
+  double bottomAnimationValue = 0;
+  double opacityAnimationValue = 0;
+  EdgeInsets paddingAnimationValue = EdgeInsets.only(top: 22);
+
+  late TextEditingController usernameController;
+  late AnimationController _animationController;
+  late Animation<Color?> _animation;
+
+  FocusNode node = FocusNode();
+  @override
+  void initState() {
+    usernameController = widget.usernameController;
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    final tween = ColorTween(begin: Colors.grey.withOpacity(0), end: blueColor);
+
+    _animation = tween.animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+    super.initState();
+
+    node.addListener(() {
+      if (node.hasFocus) {
+        setState(() {
+          bottomAnimationValue = 1;
+        });
+      } else {
+        setState(() {
+          bottomAnimationValue = 0;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        TweenAnimationBuilder<double>(
+          duration: Duration(milliseconds: 300),
+          tween: Tween(begin: 0, end: widget.fadeUsername ? 0 : 1),
+          builder: ((_, value, __) => Opacity(
+                opacity: value,
+                child: TextFormField(
+                  controller: usernameController,
+                  focusNode: node,
+                  decoration: InputDecoration(hintText: "Username"),
+                  keyboardType: TextInputType.name,
+                  onChanged: (value) async {
+                    if (value.isNotEmpty) {
+                      if (value.trim().length == 6) {
+                        setState(() {
+                          bottomAnimationValue = 0;
+                          opacityAnimationValue = 1;
+                          paddingAnimationValue = EdgeInsets.only(top: 0);
+                        });
+                        _animationController.forward();
+                      } else {
+                        _animationController.reverse();
+                        setState(() {
+                          bottomAnimationValue = 1;
+                          opacityAnimationValue = 0;
+                          paddingAnimationValue = EdgeInsets.only(top: 22);
+                        });
+                        
+                      }
+                    } else {
+                      setState(() {
+                        bottomAnimationValue = 0;
+                      });
+                      
+                    }
+                  },
+                ),
+              )),
+        ),
+        Positioned.fill(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              width: widget.fadeUsername ? 0 : 300,
+              child: TweenAnimationBuilder<double>(
+                tween: Tween(begin: 0, end: bottomAnimationValue),
+                curve: Curves.easeIn,
+                duration: Duration(milliseconds: 500),
+                builder: ((context, value, child) => LinearProgressIndicator(
+                      value: value,
+                      backgroundColor: Colors.grey.withOpacity(0.5),
+                      color: Colors.black,
+                    )),
+              ),
+            ),
+          ),
+        ),
+        Positioned.fill(
+          child: AnimatedPadding(
+            curve: Curves.easeIn,
+            duration: Duration(milliseconds: 500),
+            padding: paddingAnimationValue,
+            child: TweenAnimationBuilder<double>(
+              tween: Tween(begin: 0, end: widget.fadeUsername ? 0 : 1),
+              duration: Duration(milliseconds: 700),
+              builder: ((context, value, child) => Opacity(
+                    opacity: value,
+                    child: Align(
+                      alignment: AlignmentDirectional.centerEnd,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12.0)
+                            .copyWith(bottom: 0),
+                        child: Icon(Icons.check_rounded,
+                            size: 27,
+                            color: _animation.value // _animation.value,
+                            ),
+                      ),
+                    ),
+                  )),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+ /* bool isValidusername(String username) {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(username);
+  }*/
+}
